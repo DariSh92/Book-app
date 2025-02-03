80d678b98ffc619da2bfa095ac3b57a045826bd7
Daria Shliakhova <dshlyakhovaya1992@gmail.com>
2025-02-03 12:08:54 +0100

    addRefactoring

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { v4 as uuidv4 } from "uuid";
 import { addBook } from "../../redux/books/actionCreators";
+import createBookWithID from "../../utils/createBookWithID";
 import booksData from "../../data/books.json";
 import "./BookForm.css";
 const BookForm = () => {
@@ -13,19 +13,14 @@ const BookForm = () => {
   const handleAddRandomBook = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length);
     const randomBook = booksData[randomIndex];
-    const randomBookID = {
-      ...randomBook,
-      id: uuidv4(),
-      isFavorite: false,
-    };
+    const randomBookID = createBookWithID(randomBook);
     dispatch(addBook(randomBookID));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (title && author) {
-      dispatch(addBook({ title, author, id: uuidv4(), isFavorite: false }));
-      console.log(addBook({ title, author }));
+      dispatch(addBook(createBookWithID({ title, author })));
 
       setAuthor("");
       setTitle("");
